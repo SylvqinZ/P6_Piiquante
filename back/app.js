2a8ae5c7149360496ab50b667ce655cb18fef0ef
sylvqinZ <szanini@live.fr>
2022-08-22 18:16:12 +0200

    user / sauce

--- back/app.js
+++ back/app.js
@@ -1,11 +1,35 @@
-const express = require('express');
+const express = require("express");
+const sauceRoutes = require("./routes/sauce");
+const userRoutes = require('./routes/user');
+const path = require('path');
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://sylvain:AAuN6nHDBuQyMEGF@cluster0.ysfmhxn.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
 
-
 app.use((req, res, next) => {
-  res.json({ message: 'Coucou' });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauce', sauceRoutes);
+app.use('/api/auth', userRoutes);
 module.exports = app;
